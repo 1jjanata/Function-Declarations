88da132ca93f3363fd3374fa53bfda2e84b170dc
1jjanata <42749702+1jjanata@users.noreply.github.com>
2018-09-26 14:05:28 -0500

    Add files via upload

--- f1.js
+++ f1.js
@@ -10,7 +10,11 @@
 // (1) First Task
 // Declare a function named double() which outputs a number double the input number.
 
+function double(x){
+    let answer = 2*x;
+    return answer;
 
+}
 
 
 
@@ -19,7 +23,10 @@
 // (2) Second Task
 // Declare a function named plusplus() which outputs a number one higher than the input number.
 
-
+function plusplus(x){
+let answer = 1+x;
+    return answer;
+}
 
 
 
@@ -31,7 +38,10 @@
 
 
 
+function name(){
 
+    
+}
 
 
 
@@ -41,7 +51,10 @@
 
 
 
+function name(){
 
+    
+}
 
 
 
@@ -49,8 +62,8 @@
 // Declare a function named circleArea() which has one input named radius.
 // The function outputs the area of the circle.
 
-
-
+function circleArea(){
+let answer = 
 
 
 
@@ -59,7 +72,7 @@
 // Declare a function named boxVolume() which has three inputs: length, width, and depth.
 // The function outputs the volume of the box;
 
-
+function boxVolume(length, width, && depth){
 
 
 
@@ -73,7 +86,7 @@
 // Declare a function named thisIsMadness() which accepts one string input named city.
 // The function should output the string: "No! This is <city>!!"
 
-
+function thisIsMadness(){
 
 
 
@@ -82,7 +95,7 @@
 // (8) Eigth Task
 // Declare a function named yo() which accepts one string input named fam.
 // The function should output the string: "yo <fam>!  What's good <fam>??"
-
+function name(){
 
 
 
@@ -92,7 +105,7 @@
 // (9) Ninth Task
 // Declare a function named beefDay() which has no inputs.
 // This function should output a long string, IN ALL CAPS, of "BEEF DAY" with lots of extra E's, F's, A's, and Y's
-
+function name(){
 
 
 
@@ -106,7 +119,7 @@
 // (10) Tenth Task
 // Declare a function named xor() which has two inputs a and b.
 // This function returns the exclusive or of the two boolean inputs.
-
+function name(){
 
 
 
